73ad02de997180e7d39c061298540484d9c41583
Egemen Atik <egemenatikk@gmail.com>
2023-03-13 19:29:38 +0300

    Make the camera to follow the player

---
+++ HW#1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        transform.position = player.position + offset;
+        transform.LookAt(player);
+    }
+}
